be3fd001cadea951f06972e1b2d56dbc988e82ee
Miku Nakano <83941663+NakanoMiku13@users.noreply.github.com>
2022-12-15 23:29:55 +0000

    Update

--- Program.cs
+++ Program.cs
@@ -1,46 +1,40 @@
-﻿using System;  
-  
-class Building {   
-  private int Floors;
-  private int Area;
-  private int Occupants;
-  public Building(int Floors = 2, int Area = 1500, int Occupants = 4){
-    this.Floors = Floors;
-    this.Area = Area;
-    this.Occupants = Occupants;
-  }
-  public void setArea(int Area){
-    this.Area = Area;
-  }
-  public void setFloors(int Floors){
-    this.Floors = Floors;
-  }
-  public void setOccupants(int Occupants){
-    this.Occupants = Occupants;
-  }
-  public int getArea(){
-    return this.Area;
-  }
-  public int getOccupants(){
-    return this.Occupants;
-  }
-  public int getFloors(){
-    return this.Floors;
-  }
-  public int areaPP(){
-    return this.Area/Occupants;
-  }
+using System;
+class Node<datatype>{
+    public datatype? value;
+    public Node<datatype> next, prev;
+    public Node(datatype? value){
+        this.value = value;
+    }
+    public Node(){
+        this.value = default(datatype);
+    }
+    ~Node(){
+        this.value = default(datatype);
+    }
 }
-class BuildingDemo {   
-  static void Main() {   
-    Building house = new Building();
-    int areaPP;
-    areaPP = house.areaPP();  
-   
-    Console.WriteLine("house has:\n  " + 
-                      house.getFloors() + " floors\n  " + 
-                      house.getOccupants() + " occupants\n  " + 
-                      house.getArea() + " total area\n  " + 
-                      areaPP + " area per person"); 
-  }   
+class Stack<datatype>{
+    private Node<datatype> _head,_tail;
+    private long _size;
+    public Stack(){
+        this._head = new Node<datatype>();
+        this._tail = new Node<datatype>();
+        this._size = 0;
+    }
+    public bool isEmpty(){
+        return (this._size <= 0) ? true : false;
+    }
+    public datatype Front(){
+        return this._head.value;
+    }
+    public datatype Back(){
+        return this._tail.value;
+    }
+    public void push_front(datatype value){
+        try{
+
+        }catch(Exception ex){
+            
+        }
+    }
+
 }
